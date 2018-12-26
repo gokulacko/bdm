e2f852e8f6bdb5548d03fa12dba80702f072d201
Gokulakrishna <gokulakrishna@admins-MacBook-Pro.local>
2018-12-26 21:38:04 +0530

    static root

--- bdm/settings.py
+++ bdm/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, './dealer/static'),  # Here you tell django to look for a folder named 'assets'
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, './dealer/static'),  # Here you tell django to look for a folder named 'assets'
+    # 'static'
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "./dealer/static")
