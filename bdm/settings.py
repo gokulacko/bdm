6197e275f1f1c052152d94dc17f4c575227f3363
Gokulakrishna <gokulakrishna@admins-MacBook-Pro-2.local>
2019-01-07 20:01:30 +0530

    req password

--- bdm/settings.py
+++ bdm/settings.py
@@ -142,7 +142,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'acko',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '',
     }
